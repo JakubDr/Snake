0436bf5a525ae4aad7364b1c2c472e48a52c1008
Jakub Drastík <it2403@sspu-opava.cz>
2026-01-16 08:11:39 +0100

    Enhance BonusFood class with explanatory comments
    Added comments to explain the BonusFood class functionality.

--- js/bonus.js
+++ js/bonus.js
@@ -1,12 +1,12 @@
 class BonusFood extends Food {
   constructor() {
-    super();
-    this.timer = 300;
+    super(); // zavolá se konstruktor z Food, aby se nastavila pozice jídla
+    this.timer = 300; // jak dlouho bude bonus na mapě 
   }
 
   draw(ctx) {
-    ctx.fillStyle = "gold";
-    ctx.fillRect(this.x, this.y, 20, 20);
-    this.timer--;
+    ctx.fillStyle = "gold"; // zlatá barva, aby bylo poznat, že je to bonus
+    ctx.fillRect(this.x, this.y, 20, 20); // vykreslení bonusového jídla
+    this.timer--; // po každém vykreslení se zmenší čas, než zmizí
   }
 }
