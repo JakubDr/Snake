e7f7bd47816162393cd1334d04e1ca8f0b15d152
Jakub Drastík <it2403@sspu-opava.cz>
2026-01-16 08:19:42 +0100

    Refactor Food class with improved comments
    Updated comments in the Food class to provide clarity in Czech.

--- js/food.js
+++ js/food.js
@@ -1,17 +1,24 @@
 class Food {
+ // třída pro jídlo 
+
   constructor() {
     this.size = 20;
+    // velikost jednoho políčka
+
     this.spawn();
   }
 
   spawn() {
+    // vygeneruje náhodnou pozici 
     this.x = Math.floor(Math.random() * 25) * 20;
     this.y = Math.floor(Math.random() * 25) * 20;
   }
 
   draw(ctx) {
+    // vykreslení jídla na canvas
     ctx.fillStyle = "red";
     ctx.beginPath();
+    // kreslí kruh doprostřed políčka
     ctx.arc(this.x + 10, this.y + 10, 10, 0, Math.PI * 2);
     ctx.fill();
   }
