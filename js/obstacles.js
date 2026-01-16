f21a7e560e3aa84e1c8ec76cba2710f4513194c1
Jakub Drastík <it2403@sspu-opava.cz>
2026-01-16 09:02:07 +0100

    Refactor Obstacles class with improved comments
    Updated comments for clarity and formatting.

--- js/obstacles.js
+++ js/obstacles.js
@@ -1,21 +1,29 @@
 class Obstacles {
   constructor() {
+    // pole překážek s pevně danými souřadnicemi
     this.blocks = [
-      {x:200,y:200},{x:220,y:200},{x:240,y:200}
+      { x: 200, y: 200 },
+      { x: 220, y: 200 },
+      { x: 240, y: 200 }
     ];
   }
 
   draw(ctx) {
-    ctx.fillStyle = "gray";
-    ctx.shadowBlur = 10;
-    ctx.shadowColor = "gray";
+    // ctx je 2D kontext canvasu, pomocí něj kreslím tvary
+    ctx.fillStyle = "gray";      
+    ctx.shadowBlur = 10;         /
+    ctx.shadowColor = "gray";    
+    // vykreslí každý blok jako čtverec o velikosti 20x20
     this.blocks.forEach(b =>
       ctx.fillRect(b.x, b.y, 20, 20)
     );
-    ctx.shadowBlur = 0;
+
+    ctx.shadowBlur = 0;          // po vykreslení vypne stín
   }
 
   hit(snake) {
+    // zjistí, jestli hlava hada narazila do některé překážky
+    // vrátí true, pokud souřadnice hlavy hada odpovídají některému bloku
     return this.blocks.some(b =>
       b.x === snake.body[0].x && b.y === snake.body[0].y
     );
