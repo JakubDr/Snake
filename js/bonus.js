27580282c2bfd5053fb4a97d91e6cac43791f08f
Jakub Drastík <it2403@sspu-opava.cz>
2026-01-17 14:06:55 +0100

    Enhance BonusFood with obstacle checking on spawn
    Added obstacle-aware spawning for BonusFood.

--- js/bonus.js
+++ js/bonus.js
@@ -1,12 +1,26 @@
 class BonusFood extends Food {
-  constructor() {
-    super(); // zavolá se konstruktor z Food, aby se nastavila pozice jídla
-    this.timer = 300; // jak dlouho bude bonus na mapě 
+  constructor(obstacles = []) {
+    super(); // zavolá se konstruktor z Food
+    this.timer = 300; // jak dlouho bude bonus na mapě
+    // bezpečný spawn mimo překážky
+    this.spawn(obstacles);
   }
 
   draw(ctx) {
     ctx.fillStyle = "gold"; // zlatá barva, aby bylo poznat, že je to bonus
     ctx.fillRect(this.x, this.y, 20, 20); // vykreslení bonusového jídla
     this.timer--; // po každém vykreslení se zmenší čas, než zmizí
+  }
+
+  // spawn s kontrolou překážek
+  spawn(obstacles = []) {
+    let valid = false;
+    // opakuje generování dokud se nenajde pozice, která není na žádné překážce.
+    while (!valid) {
+      this.x = Math.floor(Math.random() * 25) * 20;
+      this.y = Math.floor(Math.random() * 25) * 20;
+
+      valid = !obstacles.some(b => b.x === this.x && b.y === this.y);
+    }
   }
 }
